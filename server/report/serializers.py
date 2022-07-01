bb228e949562c77255bdd496bd215735dac9b4b3
Hasan Zonuzi <hasanzon@yahoo.com>
2022-07-01 16:14:44 +0430

    fix: serializer fixed

--- server/report/serializers.py
+++ server/report/serializers.py
@@ -1,9 +1,11 @@
 from rest_framework import serializers
 
 from report.models import Report
+from user.serializers import UserSerializer
 
 class ReportSerializer(serializers.ModelSerializer):
-    
+    user = UserSerializer()
+    product = serializers.SlugRelatedField(slug_field='name', read_only=True)
     class Meta:
         model = Report
         fields = ['id', 'product', 'user', 'description', 'reason']
